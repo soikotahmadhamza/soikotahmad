1218e85433cb95b8b4c0e65c8fc3751ecef5c346
MD. Soikot Ahmad <saikatahmad507@gmail.com>
2026-02-28 19:13:40 +0600

    Change contact name to 'Naogaon Rajshahi Bangladesh'
    Updated contact information displayed in the component.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -26,7 +26,7 @@ const Contact: React.FC = () => {
                 <div className="p-3 bg-slate-800 rounded-lg">
                   <svg className="w-5 h-5 text-cyan-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
                 </div>
-                <span>Dhamoirhat Naogan</span>
+                <span>Naogaon Rajshahi Bangladesh</span>
               </div>
             </div>
           </div>
